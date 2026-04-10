b67aca774592e4b30c2c4899d32e41d5f6af4185
Sree <sreeiit123@gmail.com>
2026-04-10 23:19:14 +0530

    updatec Home.jsx file

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -5,7 +5,8 @@ function Home() {
   useEffect(() => {
     fetch("http://localhost:5000")
       .then(res => res.text())
-      .then(data => console.log(data));
+      .then(data => console.log(data))
+      .catch(err => console.error(err));
   }, []);
 
   return <h1>Home Page</h1>;
