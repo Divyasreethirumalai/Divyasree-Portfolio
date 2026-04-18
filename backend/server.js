994c39ce729b8dbbf35a280761c3494bfdaf7cf6
Sree <sreeiit123@gmail.com>
2026-04-18 14:45:03 +0530

    Added analytics API

--- backend/server.js
+++ backend/server.js
@@ -24,3 +24,52 @@ app.post("/track",(req,res)=>{
 });
 
 app.listen(8000,()=>console.log("Server started on 8000"));
+
+app.get("/analytics", (req, res) => {
+  fs.readFile("tracker.log", "utf-8", (err, data) => {
+    if (err) return res.send("No data");
+
+    const lines = data.trim().split("\n");
+
+    let totalVisits = lines.length;
+    let uniqueIPs = new Set();
+    let pageCount = {};
+    let visitsPerDay = {};
+
+    lines.forEach(line => {
+      const ipMatch = line.match(/IP:(.*?) /);
+      const pageMatch = line.match(/PAGE:(.*?) /);
+      const dateMatch = line.match(/\[(.*?)T/);
+
+      if (ipMatch) uniqueIPs.add(ipMatch[1]);
+
+      if (pageMatch) {
+        const page = pageMatch[1];
+        pageCount[page] = (pageCount[page] || 0) + 1;
+      }
+
+      if (dateMatch) {
+        const date = dateMatch[1];
+        visitsPerDay[date] = (visitsPerDay[date] || 0) + 1;
+      }
+    });
+
+    // find most visited page
+    let mostVisitedPage = null;
+    let max = 0;
+
+    for (let page in pageCount) {
+      if (pageCount[page] > max) {
+        max = pageCount[page];
+        mostVisitedPage = page;
+      }
+    }
+
+    res.json({
+      totalVisits,
+      uniqueVisitors: uniqueIPs.size,
+      mostVisitedPage,
+      visitsPerDay
+    });
+  });
+});
