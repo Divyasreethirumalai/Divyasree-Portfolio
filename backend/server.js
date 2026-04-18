7d6b3170ffb5f614ac65eed883cc2afa265df3cf
Sree <sreeiit123@gmail.com>
2026-04-18 14:41:50 +0530

    Implemented click tracking system

--- backend/server.js
+++ backend/server.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const fs=require("fs");
 const app=express();
 
 const cors=require("cors");
@@ -7,4 +8,19 @@ app.use(express.json());
 app.get("/",(req,res)=>{
     res.send("Server running");
 });
-app.listen(8000,()=>console.log("Server started"));
+
+
+app.post("/track",(req,res)=>{
+    console.log("Track API hit");
+    const ip=req.headers["x-forwarded-for"]||req.socket.remoteAddress;
+    const userAgent=req.headers["user-agent"];
+    const{page,event}=req.body;
+
+    const log = `[${new Date().toISOString()}] IP:${ip} PAGE:${page} UA:${userAgent} EVENT:${event}\n`;
+    fs.appendFile("tracker.log",log,(err)=>{
+        if(err) console.error(err);
+    });
+    res.send("Tracked");
+});
+
+app.listen(8000,()=>console.log("Server started on 8000"));
