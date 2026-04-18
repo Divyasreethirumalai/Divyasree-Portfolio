5b907ebe0ec742db42793d4edda1e24c6a6b90cd
Sree <sreeiit123@gmail.com>
2026-04-18 14:40:48 +0530

    Implemented click tracking system

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -3,11 +3,20 @@ import { useEffect } from "react";
 function Home() {
 
   useEffect(() => {
-    console.log("useEffect running");
-    fetch("http://localhost:8000")
-      .then(res => res.text())
-      .then(data => console.log(data))
-      .catch(err => console.error(err));
+    console.log("Tracking started");
+    fetch("http://localhost:8000/track", {
+      method:"POST",
+      headers:{
+        "Content-Type":"application/json"
+  },
+  body:JSON.stringify({
+    page:window.location.pathname,
+    event:"page_view"
+  })
+  })
+  .then(res => res.text())
+  .then(data => console.log("Response:",data))
+  .catch(err => console.error(err));
   }, []);
 
   return <h1>Home Page</h1>;
